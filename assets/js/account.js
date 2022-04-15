f840b56aa561a037ff1ab9680a61fc78a84e3aa6
KaYoZz15 <55046137+KaYoZz15@users.noreply.github.com>
2022-04-15 17:06:00 +0200

    Add files via upload

---
+++ assets/js/account.js
@@ -0,0 +1,7 @@
+<script>
+ window.onload = function(){
+         $("#menu > ul > li").hover(
+          function(){ $(this).find("ul").slideDown('fast'); } ,
+  function(){ $(this).find("ul").slideUp('fast'); } );
+ }
+</script>
