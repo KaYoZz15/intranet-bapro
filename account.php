e1e9fb9d3db0576c876e3ad336dbe4ce65c90529
KaYoZz15 <55046137+KaYoZz15@users.noreply.github.com>
2022-04-15 22:02:16 +0200

    Update account.php
    Espace mon compte
    
    Fonctionnalité :
    
    - Reset Password

--- account.php
+++ account.php
@@ -1,8 +1,9 @@
 <?php
 // Initialize the session
 session_start();
- 
-// Check if the user is logged in, if not then redirect him to login page
+
+
+// Check if the user is logged in, otherwise redirect to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
@@ -16,124 +17,193 @@ if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 
     header("location: login.php");
 }
 $_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
+ 
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$new_password = $confirm_password = "";
+$new_password_err = $confirm_password_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Validate new password
+    if(empty(trim($_POST["new_password"]))){
+        $new_password_err = "Please enter the new password.";     
+    } elseif(strlen(trim($_POST["new_password"])) < 6){
+        $new_password_err = "Password must have atleast 6 characters.";
+    } else{
+        $new_password = trim($_POST["new_password"]);
+    }
+    
+    // Validate confirm password
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Please confirm the password.";
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($new_password_err) && ($new_password != $confirm_password)){
+            $confirm_password_err = "Password did not match.";
+        }
+    }
+        
+    // Check input errors before updating the database
+    if(empty($new_password_err) && empty($confirm_password_err)){
+        // Prepare an update statement
+        $sql = "UPDATE users SET password = ? WHERE id = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "si", $param_password, $param_id);
+            
+            // Set parameters
+            $param_password = password_hash($new_password, PASSWORD_DEFAULT);
+            $param_id = $_SESSION["id"];
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Password updated successfully. Destroy the session, and redirect to login page
+                session_destroy();
+                header("location: login.php");
+                exit();
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
 ?>
 
-<!DOCTYPE HTML>
-<html>
-	<head>
-		<title>menu horizontal</title>
-		<meta charset="utf-8" />
-		<style>
-			#menu ul {
-				padding:0;
-				margin:0;
-				list-style:none;
-			}
-			#menu > ul > li {
-				float:left;
-				margin:0;
-				padding:0;
-				position:relative;
-			}
-			#menu > ul > li > a {
-			  display:block;
-			  font:normal bold 14px tahoma;
-			  background:#3f4040;
-			  min-width:100px;
-			  text-align:center;
-			  padding:10px 15px 10px 15px;
-			  text-decoration:none;
-			  color:#FFF;
-			  border-top:4px solid #FFF;
-			  border-bottom:4px solid #00B4FF;
-			  transition:background 500ms,color 500ms,border-color 500ms;
-			}
-			#menu > ul > li > a:hover {
-			  background:#666;
-			  border-top-color:#00B4FF;
-			  color:#00B4FF;
-			}
-			#menu > ul > li > ul {
-			  position:absolute;
-			  top:45px;
-			  left:0;
-			  display:none;
-			}
-			#menu > ul > li > ul > li > a {
-			  background:#666;
-			  display:block;
-			  font:normal normal 12px tahoma;
-			  padding:5px 10px 5px 10px;
-			  text-decoration:none;
-			  color:#FFF;
-			  border-bottom:1px solid #FFF;
-			  min-width:130px;
-			}
-			#menu > ul > li > ul > li > a:hover {
-				background:#777;
-			}
-		</style>
-		<script src='http://code.jquery.com/jquery-latest.js'></script>
-		<script>
-			window.onload = function(){
-				$("#menu > ul > li").hover(
-					function(){ $(this).find("ul").slideDown('fast'); } ,
-					function(){ $(this).find("ul").slideUp('fast'); } );
-			}
-		</script>
-	</head>
-	<body>
-	
-<div id="menu">
-<ul> 
 
-<li><a href="#">lien principal 1</a> <!--  -->
 
-  <ul>  <!-- menu secondaire -->
-  <li><a href="#">lien secondaire</a></li>
-  <li><a href="#">lien secondaire</a></li>
-  <li><a href="#">lien secondaire</a></li>
-  </ul>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <!--  This file has been downloaded from bootdey.com @bootdey on twitter -->
+    <!--  All snippets are MIT license http://bootdey.com/license -->
+    <title>Mon Compte</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<div class="container-xl px-4 mt-4">
+    <!-- Account page navigation-->
+    <nav class="nav nav-borders">
+        <a class="nav-link" href="index.php" target="__blank">Accueil</a>
+        <a class="nav-link active ms-0" href="account.php" target="__blank">Profil</a>
+    </nav>
+    <hr class="mt-0 mb-4">
+    <div class="row">
+        <div class="col-xl-4">
+            <!-- Profile picture card-->
+            <div class="card mb-4 mb-xl-0">
+                <div class="card-header">Mon Compte</div>
+                <div class="card-body text-center">
+                    <!-- Profile picture image-->
+                    <img class="img-account-profile rounded-circle mb-2" src="assets/images/img_avatar.png" alt="">
+                    <h5><b><?php echo htmlspecialchars($_SESSION["username"]); ?></b></h2>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-8">
+            <!-- Account details card-->
+                <!-- Change password card-->
+                <div class="card mb-4">
+                    <div class="card-header">Changer mot de passe</div>
+                    <div class="card-body">
+                                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post"> 
 
-</li>
+                        <form>
+                            <!-- Form Group (new password)-->
+                            <div class="mb-3">
+                <label>Nouveau mot de passe</label>
+                <input type="password" name="new_password" class="form-control <?php echo (!empty($new_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $new_password; ?>" placeholder="Entrer un nouveau mot de passe">
+                <span class="invalid-feedback"><?php echo $new_password_err; ?></span>
+                            </div>
+                            <!-- Form Group (confirm password)-->
+                            <div class="mb-3">
+                <label>Confirmer Mot de passe</label>
+                <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>"  placeholder="Confirmer le nouveau mot de passe">
+                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+                            </div>
+                <input type="submit" class="btn btn-primary" value="Confirmer" href="logout.php">
+                        </form>
+                    </div>
+                </div>
 
-<li><a href="#">lien principal 2</a>
+<style type="text/css">
+body{margin-top:20px;
+background-image:url(assets/images/footer-bg.png);
+color:#69707a;
+}
+.img-account-profile {
+    height: 10rem;
+}
+.rounded-circle {
+    border-radius: 50% !important;
+}
+.card {
+    box-shadow: 0 0.15rem 1.75rem 0 rgb(33 40 50 / 15%);
+}
+.card .card-header {
+    font-weight: 500;
+}
+.card-header:first-child {
+    border-radius: 0.35rem 0.35rem 0 0;
+}
+.card-header {
+    padding: 1rem 1.35rem;
+    margin-bottom: 0;
+    background-color: rgba(33, 40, 50, 0.03);
+    border-bottom: 1px solid rgba(33, 40, 50, 0.125);
+}
+.form-control, .dataTable-input {
+    display: block;
+    width: 100%;
+    padding: 0.875rem 1.125rem;
+    font-size: 0.875rem;
+    font-weight: 400;
+    line-height: 1;
+    color: #69707a;
+    background-color: #fff;
+    background-clip: padding-box;
+    border: 1px solid #c5ccd6;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    border-radius: 0.35rem;
+    transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+}
 
-  <ul>
-  <li><a href="#">lien secondaire</a></li>
-  <li><a href="#">lien secondaire</a></li>
-  </ul>
+.nav-borders .nav-link.active {
+    color: #0061f2;
+    border-bottom-color: #0061f2;
+}
+.nav-borders .nav-link {
+    color: #69707a;
+    border-bottom-width: 0.125rem;
+    border-bottom-style: solid;
+    border-bottom-color: transparent;
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+    padding-left: 0;
+    padding-right: 0;
+    margin-left: 1rem;
+    margin-right: 1rem;
+}
+</style>
 
-</li>
+<script type="text/javascript">
 
-<li><a href="#">lien principal 3</a> 
-
-  <ul> 
-  <li><a href="#">lien secondaire</a></li>
-  <li><a href="#">lien secondaire</a></li>
-  </ul>
-
-</li>
-
-<li><a href="#">lien principal 4</a> 
-
-  <ul> 
-  <li><a href="#">lien secondaire</a></li>
-  <li><a href="#">lien secondaire</a></li>
-  </ul>
-
-</li>
-
-<li><a href="#">lien principal 5</a> 
-
-  <ul> 
-  <li><a href="#">lien secondaire</a></li>
-  <li><a href="#">lien secondaire</a></li>
-  </ul>
-
-</li>
-</ul>
-</div>
-
-	</body>
+</script>
+</body>
 </html>
